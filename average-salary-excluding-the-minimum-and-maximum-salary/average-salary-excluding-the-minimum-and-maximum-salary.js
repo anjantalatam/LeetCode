d509fc42160b2ad16e54034816d7295473b2921d
Anjan Talatam <53368431+anjantalatam@users.noreply.github.com>
2023-06-07 10:18:26 +0530

    Time: 39 ms (99.75%) | Memory: 42.4 MB (15.22%) - LeetSync

---
+++ average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.js
@@ -0,0 +1,7 @@
+/**
+ * @param {number[]} salary
+ * @return {number}
+ */
+var average = function(salary) {
+    return (salary.reduce((a,b)=>a+b,0) - Math.max(...salary) - Math.min(...salary)) / (salary.length-2)
+};
