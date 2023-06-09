e841775149c33f8b225f9b1135d0b101440dc152
Anjan Talatam <53368431+anjantalatam@users.noreply.github.com>
2023-06-09 09:33:04 +0530

    Time: 70 ms (77.23%) | Memory: 43.3 MB (96.99%) - LeetSync

--- string-to-integer-atoi/string-to-integer-atoi.js
+++ string-to-integer-atoi/string-to-integer-atoi.js
@@ -3,19 +3,10 @@
  * @return {number}
  */
 var myAtoi = function(s) {
-    let str = ""
-    for(let i=0;i < s.length; i++){
-        if(parseInt(s[i]) || s[i]==="-"){
-            str += s[i]
-        }else if(s[i] !== " " && str.length === 0) {
-            str = 0
-            break;
-        }
-    }
     let output  = parseInt(s) || 0
-    if(output > 2**31 - 1){
+    if (output > 2**31 - 1){
         return 2**31 - 1
-    }else if ( output < -(2**31)){
+    } else if (output < -(2**31)){
         return -(2**31)
     }
 
