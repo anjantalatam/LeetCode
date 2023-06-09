33fd8d728923f5f2c0330114800fb44fe25bda61
Anjan Talatam <53368431+anjantalatam@users.noreply.github.com>
2023-06-09 10:19:26 +0530

    Time: 65 ms (43.30%) | Memory: 43 MB (99.08%) - LeetSync

--- check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.js
+++ check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.js
@@ -3,21 +3,14 @@
  * @return {boolean}
  */
 var checkStraightLine = function(coordinates) {
-    if(coordinates.length === 2){
-        return true;
-    }
-    
-    let store = (coordinates[1][1] - coordinates[0][1])/(coordinates[1][0] - coordinates[0][0]);
+    let slope = (coordinates[1][1] - coordinates[0][1])/(coordinates[1][0] - coordinates[0][0]);
 
     for(let i=2; i<coordinates.length; i++){
-        const [x1,y1] = coordinates[i-1]
-        const [x2,y2] = coordinates[i]
+        const sl = (coordinates[i][1] -  coordinates[i-1][1])/(coordinates[i][0] -  coordinates[i-1][0])
 
-        const slope = (y2 - y1)/(x2 - x1)
-
-        if(Math.abs(slope) === Infinity && Math.abs(store) === Math.abs(slope)){
+        if(Math.abs(sl) === Infinity && Math.abs(slope) === Math.abs(sl)){
             continue
-        }else if(store !== slope){
+        }else if(slope !== sl){
             return false
         }
     }
